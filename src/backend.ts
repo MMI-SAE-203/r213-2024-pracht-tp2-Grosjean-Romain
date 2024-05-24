cb7a3f2a141b702275263f94d769dba6b335bf53
Grosjean-Romain <romain.grosjean@edu.univ-fcomte.fr>
2024-05-24 10:11:14 +0200

    Déploiement VPS Web

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
 import PocketBase from 'pocketbase'
 import { type TypedPocketBase } from './pocketbase-types.js'
 
-export const pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase
+export const pb = new PocketBase('https://r213-tp.lifefindsaway.fr:443') as TypedPocketBase
